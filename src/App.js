604c3d822988faf6cc04258c6710c86a6ef7186d
emon al hasan <emonalhasan41@gmail.com>
2023-01-15 21:29:35 +0600

    Banner Complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Pages/Shared/Navbar';
 import {Routes,Route,Link} from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
 <Routes>
   <Route path="/" element={<Home />} />
   <Route path="about" element={<About />}/>
+  <Route path="login" element={<Login />}/>
 
-</Routes>;
+</Routes>
      
     </div>
   );
---
+++ src/Pages/Home/Info.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import InfoCard from './InfoCard';
+import clock from '../../assets/icons/clock.svg';
+import marker from '../../assets/icons/marker.svg';
+import phone from '../../assets/icons/phone.svg';
+
+const Info = () => {
+    return (
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12 mb-12'>
+            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardText="Sun-Thu...9 am -2 pm Sat-Fri...9 am -12 pm" cardTitle="Opening Hours" img={clock}></InfoCard>
+            <InfoCard bgClass="bg-accent" cardText="Mathihar,Rajshahi,Bangladesh" cardTitle="Our Locations" img={marker}></InfoCard>
+            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardText="01721-773453" cardTitle="Contact us now" img={phone}></InfoCard>
+        </div>
+    );
+};
+
+export default Info;
